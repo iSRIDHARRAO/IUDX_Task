4309435ca15e0de4527fc146a22c18419b193ac1
SRIDHAR RAO KUKKALA <50567090+iSRIDHARRAO@users.noreply.github.com>
2023-06-21 11:07:06 +0530

    Update run_in_docker.sh

--- docker/run_in_docker.sh
+++ docker/run_in_docker.sh
@@ -7,6 +7,6 @@ docker network create iudx
 
 docker volume create iudx_data
 
-docker run --name sql_container --network=iudx -p 1234:3306 -v  iudx_data:/var/lib/mysql -e MYSQL_DATABASE=iudx -e MYSQL_USER=sqluser -e MYSQL_PASSWORD=sqlpassword -e MYSQL_ROOT_PASSWORD=root -d mysql:latest
+docker run --name sql_container --network=iudx  -v  iudx_data:/var/lib/mysql -e MYSQL_DATABASE=iudx -e MYSQL_USER=sqluser -e MYSQL_PASSWORD=sqlpassword -e MYSQL_ROOT_PASSWORD=root -d mysql:latest
 
 docker run -d --name flask_app -e DEV_DATABASE_URL=mysql+pymysql://sqluser:sqlpassword@sql_container:3306/iudx -e FLASK_APP=bookshop.py --network=iudx -p 5000:5000 flask_app_api
